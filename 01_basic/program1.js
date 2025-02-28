5950721a9dcf85fb4c784cc7461cc7c596a655a6
Shankar <126222195+Web-Shankar@users.noreply.github.com>
2025-02-28 17:02:26 +0000

    console.log() & document.write() method

--- 01_basic/program1.js
+++ 01_basic/program1.js
@@ -1,3 +1,9 @@
-// Simple javascript program to print anything on server
+// Simple javascript program to print anything on server or browser console
+
+// the console.log() output the result into web browser console, browser console is a developer tool which is used to debugging & test your ConvolverNode, program etc. the following code output or print the result on browser console
+
 console.log('Hello World')
+
+// the document.write() method dynamically insert, update or delete content on HTML web page, It can be used to add text, HTML, or even JavaScript code to the page, and the output will be visible in the rendered web page. the following code print hello world on HTML webpage. 
+
 document.write('hello world 2')
